83807d948b8e7a30487b7351ceb5d6265aa991c8
AndreReliquias <andrereliquias.contato@gmail.com>
2025-03-12 19:36:28 -0300

    feat: number of pairs

---
+++ 0-number_of_pairs/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+/**
+ * @brief Excercice resolution.
+ *
+ * Calculates the number of pairs (vi, vj) of elements in the `nums` vector whose sum is `s`.
+ *
+ * @return Result.
+ */
+int resolution(int n, int s, vector<int> nums)
+{
+  int count = 0;
+  unordered_map<int, int> umap;
+
+  for (int i = 0; i < n; i++)
+  {
+    int remain = s - nums[i];
+    unordered_map<int, int>::iterator search = umap.find(remain);
+
+    if (search != umap.end())
+      count += search->second;
+
+    umap[nums[i]]++;
+  }
+
+  return count;
+}
+
+/**
+ * @brief Main function.
+ *
+ * @return `0` if sucess.
+ */
+int main()
+{
+  int n, s;
+  cin >> n >> s;
+
+  vector<int> nums(n);
+
+  for (int i = 0; i < n; ++i)
+    cin >> nums[i];
+
+  int res = resolution(n, s, nums);
+
+  cout << res << endl;
+
+  return 0;
+}
