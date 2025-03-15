0d31cf403911368164c77016f71ef6b3642f5106
AndreReliquias <andrereliquias.contato@gmail.com>
2025-03-15 17:22:28 -0300

    refactor: extract the resolution from the main

--- 1-no_time_to_code/main.cpp
+++ 1-no_time_to_code/main.cpp
@@ -5,6 +5,38 @@
 using namespace std;
 
 /**
+ * @brief Exercise resolution.
+ *
+ * Checks that the CDF is able to complete the task requested by the teacher on time within his list of subjects
+ *
+ * @return
+ */
+void resolution(unordered_map<string, int> &umap, string &task, int d, string &c)
+{
+  unordered_map<string, int>::iterator search = umap.find(task);
+
+  if (search != umap.end())
+  {
+    if (search->second <= d)
+    {
+      cout << c + "Ufa!" << endl;
+    }
+    else if (search->second > d && search->second <= d + 5)
+    {
+      cout << c + "Atrasado" << endl;
+    }
+    else
+    {
+      cout << c + "Deu ruim! Va trabalhar" << endl;
+    }
+  }
+  else
+  {
+    cout << c + "Deu ruim! Va trabalhar" << endl;
+  }
+}
+
+/**
  * @brief Main function.
  *
  * @return `0` if sucess.
@@ -33,27 +65,7 @@ int main()
     int d;
     string task;
     cin >> d >> task;
-    unordered_map<string, int>::iterator search = umap.find(task);
-
-    if (search != umap.end())
-    {
-      if (search->second <= d)
-      {
-        cout << c + "Ufa!" << endl;
-      }
-      else if (search->second > d && search->second <= d + 5)
-      {
-        cout << c + "Atrasado" << endl;
-      }
-      else
-      {
-        cout << c + "Deu ruim! Va trabalhar" << endl;
-      }
-    }
-    else
-    {
-      cout << c + "Deu ruim! Va trabalhar" << endl;
-    }
+    resolution(umap, task, d, c);
   }
 
   return 0;
--- 1-no_time_to_code/main.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void resolution(std::__1::unordered_map<std::__1::string, int> &umap, std::__1::string &task, int d, std::__1::string &c);
